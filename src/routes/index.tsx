e9aa1c98cbe70c236a197c0ba1930577998a3f4c
michaelmuthomi <lit911call@gmail.com>
2024-10-05 12:25:51 +0300

    Update favicon and page title

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -13,7 +13,7 @@ export default function LoginPage(){
       <div className="w-1/3">
         <img src={MainImage} alt="" className="h-[100%] object-cover" />
       </div>
-      <div className="grid align-center pl-40 relative w-full bg-amber-900">
+      <div className="grid align-center pl-40 relative w-full">
         <img src={Logo} alt="" className="w-14 absolute top-10 left-10" />
         <FormComponent />        
       </div>
