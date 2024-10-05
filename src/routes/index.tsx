886b7b3d16e67607794041d1f3da8b6e592e23f4
michaelmuthomi <lit911call@gmail.com>
2024-10-05 11:33:50 +0300

    refactor: login page to implement new layout
    Refactor the FormComponent and index.tsx files to improve code organization and readability.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,8 @@
 import { createFileRoute } from '@tanstack/react-router'
 import * as React from "react"
 import { FormComponent } from '../components/FormComponent'
+import Logo from "@/assets/images/gjenge.png"
+import MainImage from "@/assets/images/Gjenge.jpg"
 
 export const Route = createFileRoute('/')({
   component: () => <LoginPage />,
@@ -8,9 +10,13 @@ export const Route = createFileRoute('/')({
 
 export default function LoginPage(){
   return (
-    <section className="grid items-center h-screen">
-      <div className=" flex justify-center">
-        <FormComponent />
+    <section className="flex h-screen overflow-y-hidden">
+      <div className="w-1/3">
+        <img src={MainImage} alt="" className="h-[100%] object-cover" />
+      </div>
+      <div className="grid align-center pl-40 relative w-full">
+        <img src={Logo} alt="" className="w-14 absolute top-10 left-10" />
+        <FormComponent />        
       </div>
     </section>
   );
